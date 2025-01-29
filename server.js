234f8497dbad7e6180a650300e0d5df206d9e4e0
Ramesh Bheemanapally <129151074+ramesh8850@users.noreply.github.com>
2025-01-29 07:43:16 +0000

    added path for public and images

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "images")));
 
 app.set("view engine", "ejs"); // Ensure this line is present
 
